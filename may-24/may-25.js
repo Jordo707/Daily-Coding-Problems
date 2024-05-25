980724fe122d39ce693a9f97aae50dd5d21cd395
Jordan <penttijordo@gmail.com>
2024-05-25 16:38:45 -0400

    may 25

---
+++ may-24/may-25.js
@@ -0,0 +1,27 @@
+/*
+Given an array of numbers, find the length of the longest increasing subsequence in the array. The subsequence does not necessarily have to be contiguous.
+
+For example, given the array [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15], the longest increasing subsequence has length 6: it is 0, 2, 6, 9, 11, 15.
+*/
+
+const lengthOfLIS = (nums) => {
+    if (nums.length === 0) {
+        return 0;
+    }
+
+    const dp = new Array(nums.length).fill(1);
+
+    for (let i = 1; i < nums.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if (nums[i] > nums[j]) {
+                dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+        }
+    }
+
+    return Math.max(...dp);
+};
+
+// Example usage
+const nums = [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15];
+console.log(lengthOfLIS(nums)); // Expect 6
